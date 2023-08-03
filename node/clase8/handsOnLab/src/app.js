2bb8dac058e1acb8925b3ffad1985ea94725ec68
Enmanuel Sandoval <enmanuelsando@gmail.com>
2023-08-02 22:18:53 -0400

    Se actualiza la info de la clase 8

---
+++ node/clase8/handsOnLab/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import routerUsers from "./routes/users.js";
+import routerPets from "./routes/pets.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//Use routers
+app.use("/api/users", routerUsers);
+app.use("/api/pets", routerPets);
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Servidor arriba en el puerto ${PORT}`);
+});
---
+++ node/clase8/handsOnLab/src/routes/users.js
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import { uploader } from "../utils.js";
+
+const router = Router();
+
+const users = [
+  {
+    name: "Joaco",
+    course: "Programación Backend",
+  },
+];
+
+router.get("/", (req, res) => {
+  res.send(users);
+});
+
+router.post("/", uploader.single("file"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No se pudo guardar la imagen");
+  }
+
+  console.log(req.file);
+
+  const user = {
+    name: req.body.name ?? "Sin Nombre",
+    course: req.body.course ?? "Sin Curso",
+    profile: req.file.path,
+  };
+
+  users.push(user);
+
+  res.status(201).send("Usuario creado correctamente!");
+});
+
+export default router;
